73bf5e19b557bc7f0bde06aac1060da2f332325e
smri-merbyjuliananda <136776064+smri-merbyanda@users.noreply.github.com>
2024-01-24 16:17:10 +0800

    poc progress

--- my-ecs/src/main/java/com/example/myecs/MyECSApp.java
+++ my-ecs/src/main/java/com/example/myecs/MyECSApp.java
@@ -8,11 +8,21 @@ import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MyECSApp {
 
     private final AmazonSQS sqs;
     private final String queueUrl;
+    private final ExecutorService executorService;
+    private final BlockingQueue<Message> messageQueue;
+    private volatile boolean isShutdownRequested;
+    Integer counter = 0;
 
     public MyECSApp() {
         this.sqs = AmazonSQSClientBuilder.standard()
@@ -20,78 +30,97 @@ public class MyECSApp {
                 .withRegion("ap-southeast-1")
                 .build();
         this.queueUrl = "https://sqs.ap-southeast-1.amazonaws.com/259070268966/EcsQueue";
+        this.executorService = Executors.newFixedThreadPool(3);
+        this.messageQueue = new LinkedBlockingQueue<>();
+        this.isShutdownRequested = false;
+        // this.scheduler = Executors.newSingleThreadScheduledExecutor();
     }
 
     public void processMessage(String message) {
-        System.out.println("Processing message: " + message);
+        try {
+            System.out.println("Processing message: " + message);
+            
+            if (message.equals("test1") || message.equals("test3") || message.equals("test5")) {
+                System.out.println("message is equal to " + message + " sleeping for 2 mins");
+                Thread.sleep(120000); // sleep for 2 mins
+            } else if (message.equals("test7") || message.equals("test9")) {
+                System.out.println("message is equal to " + message + " running intensive algo");
+                intensiveCPUAlgo(message);
+            }
 
+            System.out.println("done processing message -> " + message);
+        } catch (Exception e) {
+            System.out.println("errrr" + e.getMessage());
+        }
+
+    }
+
+    public void intensiveCPUAlgo(String message) {
         // Simulate resource-intensive operation
         for (int i = 0; i < 1000000000; i++) {
             // More CPU-intensive operation (e.g., multiplying and dividing numbers)
             double result = Math.pow(i, 2) / Math.sqrt(i);
-            System.out.println("resource-intensive result of message " + message + " :" + result);
+            // System.out.println("resource-intensive result of message " + message + " :" +
+            // result);
         }
-
-        // actual processing logic
     }
 
-    public void receiveAndProcessMessages() {
-        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
-                .withQueueUrl(queueUrl)
-                .withMaxNumberOfMessages(1)
-                .withVisibilityTimeout(30) // Set an appropriate value based on your processing time
-                .withWaitTimeSeconds(20); // Set a non-zero value to enable long polling
+    public void receiveAndProcessMessagesConcurrently() {
+        while (true) {
+            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
+                    .withQueueUrl(queueUrl)
+                    .withMaxNumberOfMessages(3) // Adjust the batch size to the number of threads
+                    .withVisibilityTimeout(120)
+                    .withWaitTimeSeconds(20);
 
-        System.out.println("receiveMessageRequest - " + receiveMessageRequest);
+            List<Message> receivedMessages = sqs.receiveMessage(receiveMessageRequest).getMessages();
 
-        List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+            // Put received messages into the blocking queue
+            messageQueue.addAll(receivedMessages);
+            if (receivedMessages.size() == 0) {
+                System.out.println("No messages in the SQS queue.");
+            } else {
+                // System.out.println("Received messages: " + receivedMessages);
+                System.out.println("receivedMessages.size() - " + receivedMessages.size());
+                for (int i = 0; i < Math.min(receivedMessages.size(), 3); i++) {
+                    executorService.submit(() -> {
+                        try {
+                            // Dequeue a message from the blocking queue
+                            Message receivedMessage = messageQueue.take();
 
-        System.out.println("Received messages: " + messages);
+                            String messageBody = receivedMessage.getBody();
+                            String receiptHandle = receivedMessage.getReceiptHandle();
+                            // System.out.println("receivedMessage -> " + receivedMessage);
+                            System.out.println("executorService.submit(() -> counter " + counter);
+                            System.out.println("messageBody: " + messageBody);
 
-        for (Message receivedMessage : messages) {
+                            // Process the received message
+                            processMessage(messageBody);
 
-            String messageBody = receivedMessage.getBody();
-            String receiptHandle = receivedMessage.getReceiptHandle();
+                            // Delete received message from queue
+                            sqs.deleteMessage(new DeleteMessageRequest(queueUrl, receiptHandle));
+                            System.out.println("message " + messageBody + " deleted");
 
-            System.out.println("messageBody: " + messageBody);
-            System.out.println("receiptHandle: " + receiptHandle);
-
-            try {
-                // Process the received message
-                processMessage(messageBody);
-
-                // Delete received message from queue
-                sqs.deleteMessage(new DeleteMessageRequest(queueUrl, receiptHandle));
-            } catch (Exception e) {
-                System.err.println("Error processing message: " + e.getMessage());
-                // can throw the exception to trigger a retry later on if needed
+                        } catch (Exception e) {
+                            System.out.println("Error processing message: " + e.getMessage());
+                            Thread.currentThread().interrupt();
+                        }
+                    });
+                }
             }
         }
+        // Gracefully shut down the executorService
+        // executorService.shutdown();
+    }
 
-        if (messages.isEmpty()) {
-            System.out.println("No messages in the SQS queue.");
-        }
+    public void shutdown() {
+        isShutdownRequested = true;
     }
 
     public static void main(String[] args) {
         System.out.println("Starting my ECS app POC!");
-        System.out.println("change 3 log test");
         MyECSApp ecsTask = new MyECSApp();
-        int counter = 0;
-        // Run continuously
-        boolean keepRunning = true;
-        while (keepRunning) {
-            counter++;
-            System.out.println("Running receiveAndProcessMessages...->" + counter);
-            ecsTask.receiveAndProcessMessages();
-            try {
-                // Sleep for some time before polling again
-                Thread.sleep(10000); // message polling interval: 10 seconds
-            } catch (InterruptedException e) {
-                // Handle interruptions gracefully
-                Thread.currentThread().interrupt();
-                keepRunning = false;
-            }
-        }
+        ecsTask.receiveAndProcessMessagesConcurrently();
+        System.out.println("exiting task");
     }
 }
